acdff9b375e6d685cec6e486d9907c797daf9390
Ariel Valle <avallehh@gmail.com>
2019-07-30 20:40:29 -0400

    modified html and style sheet

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCdMvx-v5pzD_0V9iyl9G14DdXQcPbNsEU",
+    authDomain: "trainscheduler-c5168.firebaseapp.com",
+    databaseURL: "https://trainscheduler-c5168.firebaseio.com",
+    projectId: "trainscheduler-c5168",
+    storageBucket: "",
+    messagingSenderId: "768778725533",
+    appId: "1:768778725533:web:bf354baa0ab1785b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
